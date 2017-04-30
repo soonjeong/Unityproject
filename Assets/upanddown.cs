0efbb2606933b0e14a196c96e47547b29d635b1f
soonjeong <woak_uu@naver.com>
2017-04-30 16:43:59 +0900

    0430
    클릭시 튀어오르기 동작

---
+++ Assets/upanddown.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class upanddown : MonoBehaviour {
+    public float upvar;
+    public float downvar;
+    public float xrange;
+    public float yrange;
+   
+    private object newPosition;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(Input.GetMouseButtonDown(0))
+        {    Vector3 newPosition = transform.position;
+            Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Mathf.Abs(mouse.x - newPosition.x) < xrange &&
+                Mathf.Abs(mouse.y - newPosition.y) < yrange)
+            { newPosition.y += upvar * Time.deltaTime;
+
+            transform.position = newPosition;}
+            
+
+            
+        }
+		
+        if(Input.GetMouseButtonUp(0))
+        {
+            Vector3 newPosition = transform.position;
+            Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Mathf.Abs(mouse.x - newPosition.x) < xrange &&
+                Mathf.Abs(mouse.y - newPosition.y) < yrange)
+            { newPosition.y += downvar * Time.deltaTime;
+
+            transform.position = newPosition;}
+                
+        }
+	}
+}
