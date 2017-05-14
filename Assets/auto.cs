f04ebebbad3afa6a9463e8fa85860ccf56bafd2f
soonjeong <woak_uu@naver.com>
2017-05-14 16:32:00 +0900

    automatic movement
    자동 움직임 스크립트 작성
    이후 판단 스크립트

---
+++ Assets/auto.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class auto : MonoBehaviour {
+
+    public List<GameObject> Chickens;
+    public List<int> MoveSequence = new List<int>();
+    public int Length;
+    int NowChicken = 0;
+    public float time = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartAutoMove();
+        Chickens[MoveSequence[NowChicken]].GetComponent<examplemovement>().MoveStart();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+      time += Time.deltaTime;
+        if(time > 4.0f )
+        {
+            time = 0.0f;
+            NowChicken = NowChicken + 1;
+            Chickens[MoveSequence[NowChicken]].GetComponent<examplemovement>().MoveStart();
+        }
+	}
+
+    public void StartAutoMove()
+    {
+        
+
+        MoveSequence.Clear();
+        for(int i=0; i < Length; i++)
+        {
+            MoveSequence.Add(Random.Range(0, 3));
+        }
+       
+    }
+}
---
+++ Assets/examplemovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class examplemovement : MonoBehaviour {
+    public float upanddown;
+    //0 = 올라감, 1 =대기, 2 = 내려옴, 3 = 아무것도 안함
+    public int state = 3;
+    public float time = 0.0f;
+    
+	// Use this for initialization
+	void Start () {
+        
+
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time += Time.deltaTime;
+        if(time > 1.0f)
+        {
+            time = 0.0f;
+            state = state + 1;
+        }
+
+        if (state == 0)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.y += Time.deltaTime * upanddown;
+            transform.position = newPosition;
+
+        }
+        else if(state == 1)
+        {
+
+        }
+        else if(state==2)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.y -= Time.deltaTime * upanddown;
+            transform.position = newPosition;
+        }
+        else if(state==3)
+        {
+               
+
+        }
+    }
+
+    public void MoveStart()
+    {
+        state = 0;
+        time = 0.0f;
+    }
+}
