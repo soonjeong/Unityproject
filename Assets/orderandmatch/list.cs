79e79cc2543f4827f26e684466b6d32f03724ce0
lee chaehun <littrd3@gmail.com>
2017-07-16 16:52:06 +0900

    match

--- Assets/orderandmatch/list.cs
+++ Assets/orderandmatch/list.cs
@@ -5,36 +5,105 @@ using UnityEngine.UI;
 
 public enum function
 {
-    product,
-    sum,
+    order,
+    follow,
     match,
-    success.
+    success,
     fail
     
 }
+[System.Serializable]
+public class boxes
+{
+    public string name;
+    public GameObject box;
+    public int values;
+   
+    
+}
 public class list : MonoBehaviour {
-    public List<GameObject> colorboxes;
-    public List<int> colornumber = new List<int>();
+    public List<boxes> colorboxes;
     public GameObject order;
     public GameObject answer;
     public string commend;
-    public string box1;
-    public string box2;
-    
+    public int mixedvalue;
+    public function phase = function.order;
+    public string kakikomu;
+    public int index1;
+    public int index2;
 
 	// Use this for initialization
 	void Start () {
-        colornumber.Clear();
-        colornumber.Add(Random.Range(0, 7));
 
+        index1 = Random.Range(0, 7);
+        index2 = Random.Range(0, 7);
+        
+        phase = function.order;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        order.GetComponent<Text>().text = box1+"과"+box2+"를"+commend+"주세요";
+        if (phase == function.order)
+        {
+            numdis();
+        }
+        if (phase == function.match)
+        {
+              compare();
+        }
+        if (phase == function.follow)
+        {
+            storing();
+        }
+        if (phase == function.fail) {
+            order.GetComponent<Text>().text = "다시 해봅시다";
 
+                }
+        if (phase == function.success)
+        {
+            order.GetComponent<Text>().text = "정답이에요~!";
+        }
+        
+    
 	}
 
+    //함수들
+    public void numdis()
+    {
+        order.GetComponent<Text>().text = colorboxes[index1].name + "과" + colorboxes[index2].name + "를" + "더해주세요";
+        mixedvalue = colorboxes[index1].values + colorboxes[index2].values;
+        phase = function.follow;
+    }
 
+    public void storing()
+    {
+        kakikomu = answer.GetComponent<Text>().text;
+        
+    }
+
+    public void compare() {
+        if (kakikomu == mixedvalue.ToString())
+        {
+            phase = function.success;
+        }
+
+        else
+        {
+            phase = function.fail;
+        }
+    }
+    public void reset() {
+
+        if (phase == function.success || phase == function.fail)
+        {
+            index1 = Random.Range(0, 7);
+            index2 = Random.Range(0, 7);
+            phase = function.order;
+        }
+         
+        
+    }
+
+      
 }
